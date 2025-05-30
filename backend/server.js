0a8e2938af448acf5fdc1d48f9b70b803e943811
anubhavraiii <realar99@gmail.com>
2025-05-30 23:28:29 +0530

    second attempt

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
     app.get("/*", (req, res, next) => {
-        if (req.path.startsWith("/api")) return next();
+        //if (req.path.startsWith("/api")) return next();
         res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
     });
 }
