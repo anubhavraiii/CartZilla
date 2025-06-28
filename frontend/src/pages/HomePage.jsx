faa8b291d137159074b50f565464170b8ecf7a48
ANUBHAV RAI <144589308+anubhavraiii@users.noreply.github.com>
2025-06-29 01:08:03 +0530

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -27,7 +27,7 @@ const HomePage = () => {
 					Explore Our Categories
 				</h1>
 				<p className='text-center text-xl text-gray-300 mb-12'>
-					Discover the latest trends in eco-friendly fashion
+					Look good. Feel good. Do good — shop our latest trends
 				</p>
 
 				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
